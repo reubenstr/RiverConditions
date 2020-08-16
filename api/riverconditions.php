c8be0154e6fdc243e5b0b485b6f65c4700c95603
Reuben <reubenstrangelove@gmail.com>
2020-08-16 10:02:14 -0400

    Update riverconditions.php

--- api/riverconditions.php
+++ api/riverconditions.php
@@ -1,5 +1,4 @@
 <?php
-
 // Captures river conditions from waterreporter.org and waterservices.usgs.gov
 // Data is retreived in JSON, parsed, reduced, and repacked as JSON into a managable size.
 // JSON data is cached as files where the file name is the station id.
@@ -7,52 +6,84 @@
 // Aug. 13th 2020
 //
 // Usage example: www.hostingwebsite.com/api/riverconditions.php?stationId=20209000
-
-set_error_handler("warning_handler", E_WARNING | E_ALL);
-
-$stationId = htmlspecialchars($_GET["stationId"]);
-
-
-echo get_json($stationId);
-
-// End
-
-function get_json($stationId)
+//set_error_handler("warning_handler", E_WARNING | E_ALL);
+// Check for present and valid station parameters.
+if (isset($_GET['usgs']) && !empty($_GET['usgs']))
 {
-    $cacheFile = 'cache' . DIRECTORY_SEPARATOR . $stationId . '.json';
+    $usgsId = htmlspecialchars($_GET["usgs"]);
+}
+else
+{
+    $usgsId = null;
+}
 
+if (isset($_GET['wr']) && !empty($_GET['wr']))
+{
+    $wrId = htmlspecialchars($_GET["wr"]);
+}
+else
+{
+    $wrId = null;
+}
+
+//
+if ($usgsId != null)
+{
+    $usgsJson = get_json("USGS", $usgsId);
+    //$usgsJson = file_get_contents('supplementUSGS.json'); // TEMP    
+}
+else
+{
+    $usgsJson = null;
+}
+
+if ($wrId != null)
+{
+    $wrJson = get_json("WR", $wrId);
+    //$wrJson = file_get_contents('supplement.json'); // TEMP    
+}
+else
+{
+    $wrJson = null;
+}
+
+$jsonComplete = parse_station_json($usgsJson, $wrJson);
+
+echo $jsonComplete;
+
+// End script.
+
+
+function get_json($stationType, $stationId)
+{    
+	$cacheFile = 'cache' . DIRECTORY_SEPARATOR . $stationId . '.json';
+
+    /**/
     // Check if json exists in cache and is valid.
     if (file_exists($cacheFile))
-    {    
-        /*
-        if (filemtime($cacheFile) > strtotime('-60 minutes'))
+    {
+        if (filemtime($cacheFile) > strtotime('-600 minutes'))
         {
             $fh = fopen($cacheFile, 'r');
-            
-        if (filesize($cacheFile) != 0)
+
+            if (filesize($cacheFile) != 0)
             {
-        return fread($fh, filesize($cacheFile));
-            }			            
+                return fread($fh, filesize($cacheFile));
+            }
         }
-        */
     }
 
-    // Get json from endpoint then save in cache.
-       
-	
-	$json_source = "USGS";
+    // Get json from endpoint, save json into cache file.
+    if ($stationType == "USGS")
+    {
+        $url = "https://waterservices.usgs.gov/nwis/iv/?format=json&variable=00060,00065&sites={$stationId}";
+    }
+    else if ($stationType == "WR")
+    {
+        $url = "https://stations.waterreporter.org/{$stationId}/supplement.json";
+    }
 
-/*
-	if ($json_source == "WR")
-	{
-		$url = 'http://worldtimeapi.org/api/timezone/Europe';
-	}
-	else
-	{
-		 $url = "https://waterservices.usgs.gov/nwis/iv/?format=json&sites={$stationId}&parameterCd=00060,00065";
-	}   
-    
-	$result = get_web_page($url);
+    $result = get_web_page($url);
 
     if ($result['errno'] != 0)
     {
@@ -64,127 +95,114 @@ function get_json($stationId)
         error('http code: ' . $result['http_code']);
     }
 
-    $json = parse_json_from_endpoint($result['content']);
-	 $array = json_decode($json);
-  */
 
-    // TEMP
-    $array = json_decode(file_get_contents('supplementUSGS.json') , true);
-	// TEMP
-	
-	if (json_last_error() != JSON_ERROR_NONE)
-	{
-		error(json_last_error_msg());
-	}
-	
-    $json = parse_json_from_endpoint($json_source, $array);
-
+    // Cache station json to file system.
     $fh = fopen($cacheFile, 'w');
-    fwrite($fh, $json);
+    fwrite($fh, $result['content']);
     fclose($fh);
 
-    return $json;
+    return $result['content'];
 }
 
-function parse_json_from_endpoint($json_source, $json)
+function parse_station_json($usgsJson, $wrJson)
 {
 
-$array = array();
+    // Init variables.
+    $array = array();
 
-$noData = -9; // Per waterreporter usage.
+    $usgsId = "";
+    $wrId = "";
+    $usgsName = "";
+    $wrName = "";
+    $wrIsActive = "";
+    $usgsDescription = "";
+    $wrDescription = "";
+    $recordTime = date(DateTime::ISO8601);
 
- $bacteria_threshold_date_time = "";
-        $bacteria_threshold_value = $noData;
+    $noData = - 9; // Per waterreporter usage.
+    $bacteria_threshold_date_time = "";
+    $bacteria_threshold_value = $noData;
 
-        $water_temp_c_date_time = "";
-        $water_temp_c_value = $noData;
+    $water_temp_c_date_time = "";
+    $water_temp_c_value = $noData;
 
-        $e_coli_concentration_date_time = "";
-        $e_coli_concentration_value = $noData;
-		
-		 $stream_flow = "";
-		$stream_flow_date_time = $noData;
+    $e_coli_concentration_date_time = "";
+    $e_coli_concentration_value = $noData;
    
-		$gauge_height = "";
-		$gauge_height_date_time = $noData;
+    $stream_flow_date_time = "";
+	 $stream_flow_value = $noData;
+   
+    $gauge_height_date_time = "";
+	 $gauge_height_value = $noData;
+	
+    if ($usgsJson != null)
+    {
+        $usgsArray = json_decode($usgsJson, true);
 
+        if (json_last_error() != JSON_ERROR_NONE)
+        {
+            error("USGS json error: " . json_last_error_msg());
+        }
 
-    if ($json_source == "USGS")
-    { 
-        $id = $json['value']['timeSeries'][0]['sourceInfo']['siteCode'][0]['value'];
-        $name = $json['value']['timeSeries'][0]['sourceInfo']['siteName'];
-        $is_active = true;
-        $description = "USGS stream monitoring site.";
-		$recordTime = date(DateTime::ISO8601);
-      
-	  // TODO: test if keys exist.
-        $variable_count = count($json['value']['timeSeries']);
+        $usgsId = $usgsArray['value']['timeSeries'][0]['sourceInfo']['siteCode'][0]['value'];
+        $usgsName = $usgsArray['value']['timeSeries'][0]['sourceInfo']['siteName'];
+        $usgsDescription = "USGS stream monitoring site.";
+
+        $variable_count = count($usgsArray['value']['timeSeries']);
 
         for ($i = 0;$i < (int)$variable_count;$i++)
         {
-            if ("00060" == $json['value']['timeSeries'][$i]['variable']['variableCode'][0]['value'])
+            if ("00060" == $usgsArray['value']['timeSeries'][$i]['variable']['variableCode'][0]['value'])
             {
-                $stream_flow = $json['value']['timeSeries'][$i]['values'][0]['value'][0]['value'];
-                $stream_flow_date_time = $json['value']['timeSeries'][$i]['values'][0]['value'][0]['dateTime'];
+                $stream_flow_value = $usgsArray['value']['timeSeries'][$i]['values'][0]['value'][0]['value'];
+                $stream_flow_date_time = $usgsArray['value']['timeSeries'][$i]['values'][0]['value'][0]['dateTime'];
             }
-            if ("00065" == $json['value']['timeSeries'][$i]['variable']['variableCode'][0]['value'])
+            if ("00065" == $usgsArray['value']['timeSeries'][$i]['variable']['variableCode'][0]['value'])
             {
-                $gauge_height = $json['value']['timeSeries'][$i]['values'][0]['value'][0]['value'];
-                $gauge_height_date_time = $json['value']['timeSeries'][$i]['values'][0]['value'][0]['dateTime'];
+                $gauge_height_value = $usgsArray['value']['timeSeries'][$i]['values'][0]['value'][0]['value'];
+                $gauge_height_date_time = $usgsArray['value']['timeSeries'][$i]['values'][0]['value'][0]['dateTime'];
             }
-        }   
+        }
     }
-    else if ($json_source == "WR")
-    {		
-        // Get the index of most recent record (records are formatted in an array).
-        $bacteria_threshold_recent_index = count($json['sample_idx']['bacteria_threshold']) - 1;
-        $water_temp_c_recent_index = count($json['sample_idx']['water_temp_c']) - 1;
-        $e_coli_concentration_recent_index = count($json['sample_idx']['e_coli_concentration']) - 1;
-
-        $id = $json['station']['id'];
-        $name = $json['station']['name'];
-        $is_active = $json['station']['is_active'];
-        $description = $json['station']['description'];
-
-        $bacteria_threshold_date_time = $json['sample_idx']['bacteria_threshold'][$bacteria_threshold_recent_index]['date'];
-        $bacteria_threshold_value = $json['sample_idx']['bacteria_threshold'][$bacteria_threshold_recent_index]['value'];
-
-        $water_temp_c_date_time = $json['sample_idx']['water_temp_c'][$water_temp_c_recent_index]['date'];
-        $water_temp_c_value = $json['sample_idx']['water_temp_c'][$water_temp_c_recent_index]['value'];
-
-        $e_coli_concentration_date_time = $json['sample_idx']['e_coli_concentration'][$e_coli_concentration_recent_index]['date'];
-        $e_coli_concentration_value = $json['sample_idx']['e_coli_concentration'][$e_coli_concentration_recent_index]['value'];
-    }
-	
-	// Get static station data from stations.json
-	 $stationFile = 'stations.json';
-    if (file_exists($stationFile))
-    { 
-      $stationArray = json_decode(file_get_contents($stationFile) , true); 
-	 
-		if (!isset($stationArray['stations'][$id]))
-		{
-			error("Station with stationId of {$id} not found in {$stationFile}");
-		}  
-	  
-		$shortName = $stationArray['stations'][$id]['shortName'];
-		$location = $stationArray['stations'][$id]['location'];
-    }
-	else
-	{
-		error("File not found: stations.json");
-	}	
-	
     
+	if ($wrJson != null)
+    {
+        $wrArray = json_decode($wrJson, true);
+
+        if (json_last_error() != JSON_ERROR_NONE)
+        {
+            error("WR json error: " . json_last_error_msg());
+        }
+
+        // Get the index of most recent record (records are formatted in an array).
+        $bacteria_threshold_recent_index = count($wrArray['sample_idx']['bacteria_threshold']) - 1;
+        $water_temp_c_recent_index = count($wrArray['sample_idx']['water_temp_c']) - 1;
+        $e_coli_concentration_recent_index = count($wrArray['sample_idx']['e_coli_concentration']) - 1;
+
+        $wrId = strval($wrArray['station']['id']);
+        $wrName = $wrArray['station']['name'];
+        $wrIsActive = $wrArray['station']['is_active'];
+        $wrDescription = $wrArray['station']['description'];
+
+        $bacteria_threshold_date_time = $wrArray['sample_idx']['bacteria_threshold'][$bacteria_threshold_recent_index]['date'];
+        $bacteria_threshold_value = $wrArray['sample_idx']['bacteria_threshold'][$bacteria_threshold_recent_index]['value'];
+
+        $water_temp_c_date_time = $wrArray['sample_idx']['water_temp_c'][$water_temp_c_recent_index]['date'];
+        $water_temp_c_value = $wrArray['sample_idx']['water_temp_c'][$water_temp_c_recent_index]['value'];
+
+        $e_coli_concentration_date_time = $wrArray['sample_idx']['e_coli_concentration'][$e_coli_concentration_recent_index]['date'];
+        $e_coli_concentration_value = $wrArray['sample_idx']['e_coli_concentration'][$e_coli_concentration_recent_index]['value'];
+    }
+
     $array['station'] = array(
-        'id' => $id,
-        'name' => $name,
-		'shortName' => $shortName,
-		'location' => $location,
-        'isActive' => $is_active,
-        'description' => $description,
-		'recordTime' => $recordTime
-		
+        'usgsId' => $usgsId,
+        'wrId' => $wrId,
+        'usgsName' => $usgsName,
+        'wrName' => $wrName,
+        'wrIsActive' => $wrIsActive,
+        'usgsDescription' => $usgsDescription,
+        'wrDescription' => $wrDescription,
+        'recordTime' => $recordTime
     );
 
     $array['data'] = array(
@@ -202,15 +220,15 @@ $noData = -9; // Per waterreporter usage.
         ) ,
         'streamFlow' => array(
             'date' => $stream_flow_date_time,
-            'value' => intval($stream_flow)
-        ),
-		'gaugeHeight' => array(
+            'value' => intval($stream_flow_value)
+        ) ,
+        'gaugeHeight' => array(
             'date' => $gauge_height_date_time,
-            'value' => intval($gauge_height)
+            'value' => intval($gauge_height_value)
         )
-    );	
-	
-    return json_encode($array, JSON_PRETTY_PRINT); 
+    );
+
+    return json_encode($array, JSON_PRETTY_PRINT);
 }
 
 function error($msg)
